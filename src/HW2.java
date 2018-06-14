acb86b39ef692e981dc8da38cc30d3ace402c4dc
Houman Sarabostani <sarabostani@outlook.com>
2018-06-14 11:51:58 -0700

    tabs to spaces

--- src/HW2.java
+++ src/HW2.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class HW2 {
 
-	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
-		System.out.println("Please enter your age");
-		int age = reader.nextInt();
-		if (age <= 0) {
-			System.out.println("That's not correct.");
-		} else {
-			System.out.println("You are " + age + " years old.");
-		}
-		
-		reader.close();
-	}
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please enter your age");
+        int age = reader.nextInt();
+        if (age <= 0) {
+            System.out.println("That's not correct.");
+        } else {
+            System.out.println("You are " + age + " years old.");
+        }
+
+        reader.close();
+    }
 }
--- src/lesson_4_loops/Lesson4.java
+++ src/lesson_4_loops/Lesson4.java
@@ -24,20 +24,20 @@ import java.util.Scanner;
  * */
 
 public class Lesson4 {
-	public static void main(String[] args) {
-		Scanner read = new Scanner(System.in);
-		int index = 1;
-		
-		/*while( index <= 10 ) {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int index = 1;
+
+        /*while( index <= 10 ) {
 			System.out.println(index);
 			if( index % 7 == 0) {
 				break;
 			}
 			index++;
 		}
-		
+
 		int value = 0;
-		
+
 		while(true) {
 			System.out.println("Enter value");
 			value = read.nextInt();
@@ -45,67 +45,67 @@ public class Lesson4 {
 				break;
 			}
 			System.out.println("you entered " + value);
-			
+
 		}*/
-		
-		/*for(int i = 0; i < 10; i++) {
+
+        /*for(int i = 0; i < 10; i++) {
 			System.out.print(i + " ");
 			// nested loop
 			for(int j = 1; j <= 4; j++) {
 				System.out.print(j + " ");
 			}
-			
+
 			System.out.println("");
 		}*/
-		
-		// arrays or vectors
-		// [][][][][][][][][][][][][][][]
-		
-		// syntax
-		/*double[] heights = new double[10];
-		
+
+        // arrays or vectors
+        // [][][][][][][][][][][][][][][]
+
+        // syntax
+        /*double[] heights = new double[10];
+
 		heights[0] = 32;
 		heights[1] = 10;
 		heights[2] = 11;
-		
+
 		System.out.println(heights[3]);*/
-		
-		/*for( int i = 0; i < heights.length; i++) {
+
+        /*for( int i = 0; i < heights.length; i++) {
 			heights[i] = Math.sin(i*2);
 		}*/
-		
-		//System.out.println(heights[9]);
-		
-		/*for( int i = 0; i < heights.length; i++) {
+
+        //System.out.println(heights[9]);
+
+        /*for( int i = 0; i < heights.length; i++) {
 			System.out.print(heights[i] + " ");
 		}*/
-		
-		for( int i = 1; i <= 8; i++) {
-			for(int j = 1; j <= i; j++) {
-				System.out.print("*");
-			}
-			System.out.println("");
-		}
-		
-		String name = "Houman";
-		
-		name.compareTo("Houman");
-		
-		if(name == "houman") {
-			System.out.println("You're awesome");
-		}
-		
-		name.charAt(0);
-		
-		char[] lastName = new char[3]; // "Lee"
-		
-		lastName[0] = 'L';
-		lastName[1] = 'e';
-		lastName[2] = 'f';
-		System.out.println("");
-		for(int i = 0; i < lastName.length; i++) {
-			System.out.print(lastName[i]);
-		}
-		
-	}
+
+        for( int i = 1; i <= 8; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        String name = "Houman";
+
+        name.compareTo("Houman");
+
+        if(name == "houman") {
+            System.out.println("You're awesome");
+        }
+
+        name.charAt(0);
+
+        char[] lastName = new char[3]; // "Lee"
+
+        lastName[0] = 'L';
+        lastName[1] = 'e';
+        lastName[2] = 'f';
+        System.out.println("");
+        for(int i = 0; i < lastName.length; i++) {
+            System.out.print(lastName[i]);
+        }
+
+    }
 }
